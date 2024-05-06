41f736e9aebc468cee8a522bdc6a4d84807f638d
grush <grush@DESKTOP-P5A2MM5>
2024-05-06 12:35:46 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Lab11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    int* ptrA, * ptrB;
+
+
+    cout << "Enter the value for a: ";
+    cin >> a;
+    cout << "Enter the value for b: ";
+    cin >> b;
+
+
+    ptrA = &a;
+    ptrB = &b;
+
+
+    *ptrA *= 4;
+
+
+    *ptrB /= 5;
+
+
+    cout << "Updated value for a: " << a << endl;
+    cout << "Updated value for b: " << b << endl;
+
+    return 0;
+}
